6feaf634777e7539bbfcdf3a9bbaf2def243186b
MuXeD <juanan.sm87@gmail.com>
2023-11-04 17:27:47 +0100

    ⚰️ Update sample view

--- src/views/SampleView.vue
+++ src/views/SampleView.vue
@@ -1,6 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
 <template>
-  <img alt="Vue logo" src="../assets/Logo_MuXeD.png" />
   <HelloWorld msg="Hello Vue 3 Template" />
 </template>
