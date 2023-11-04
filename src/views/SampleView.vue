0bb0c2779ea6b947bde7ffd13c6de76e5cca5304
MuXeD <juanan.sm87@gmail.com>
2023-11-04 22:02:32 +0100

    🚧 Temporal view

--- src/views/SampleView.vue
+++ src/views/SampleView.vue
@@ -1,6 +1,15 @@
 <script setup lang="ts">
+import GenericView from 'src/components/common/GenericView.vue';
+import { useWeapons } from 'src/composables/client/UseWeapons';
+import { GenericViewKey } from 'src/types/Symbols';
+
+const weapons = useWeapons();
+
+provide(GenericViewKey, {
+  tableService: weapons.getWeaponsPaginated
+})
 </script>
 
 <template>
-  <HelloWorld msg="Hello Vue 3 Template" />
+  <GenericView />
 </template>
