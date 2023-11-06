137f60b7d9371956bdec6b2f639d8c5978c717cb
MuXeD <juanan.sm87@gmail.com>
2023-11-06 18:07:42 +0100

    🌐 New translations

--- src/locales/en/en.ts
+++ src/locales/en/en.ts
@@ -1,11 +1,21 @@
 import menu from './menu.json';
+import success from './success.json';
+import warnings from './warnings.json';
+import rules from './rules.json';
 import titles from './common/titles.json';
 import messages from './common/messages.json';
+import labels from './common/labels.json';
+import buttons from './common/buttons.json';
 
 export default {
   menu,
+  success,
+  warnings,
+  rules,
   common: {
     titles,
     messages,
+    labels,
+    buttons,
   }
 };
