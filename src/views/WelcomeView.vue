7610dee45657e3da1dc745b58c14e16fe9f6a6e4
juananmuxed <juanan.sm87@gmail.com>
2023-12-14 19:44:29 +0100

    🩹 Add emojis

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -2,21 +2,21 @@
   <QCard class="q-ma-xl">
     <QCardSection>
       <h3>⚔️ {{ $t("common.titles.adminForClash") }} ⚔️</h3>
-      <h5>{{ $t("common.titles.welcome") }}</h5>
+      <h5>🎱 {{ $t("common.titles.welcome") }}</h5>
       <p v-html="$t('common.texts.welcome01')"></p>
       <p v-html="$t('common.texts.welcome02')"></p>
-      <h5>{{ $t("common.titles.expansionsAvailable") }}</h5>
+      <h5>⛩ {{ $t("common.titles.expansionsAvailable") }}</h5>
       <ul>
         <li>Clash of Spears</li>
         <li>Fields of Blood</li>
         <li>Rise of Eagles</li>
         <li>Clash of Katanas</li>
       </ul>
-      <h5>{{ $t("common.titles.componentsWebs") }}</h5>
+      <h5>🎲 {{ $t("common.titles.componentsWebs") }}</h5>
       <ul>
         <li v-html="$t('common.texts.webs01')"></li>
       </ul>
-      <h5>{{ $t("common.titles.news") }}</h5>
+      <h5>📰 {{ $t("common.titles.news") }}</h5>
       <ul>
         <li v-html="$t('common.texts.news01')"></li>
       </ul>
